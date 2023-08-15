851223d72e4af7c2a41380a87c7d9d3bf92c2c4a
ahadcove <me@ahadcove.com>
2023-08-15 10:36:41 -0400

    chore: version bump

--- app.js
+++ app.js
@@ -7,40 +7,39 @@ const log_stdout = process.stdout;
 
 // Log to a file
 console.log = function (d) {
-  log_file.write(util.format(d) + "\n");
-  log_stdout.write(util.format(d) + "\n");
+  log_file.write(JSON.stringify(d) + "\n");
+  log_stdout.write(JSON.stringify(d) + "\n");
 };
 console.error = function (d) {
-  log_file.write(util.format(d) + "\n");
-  log_stdout.write(util.format(d) + "\n");
+  log_file.write(JSON.stringify(d) + "\n");
+  log_stdout.write(JSON.stringify(d) + "\n");
 };
 
 const corsair = new CorsairApi();
 
 const start = async () => {
   try {
-    console.log("Starting");
     const res = await corsair.setup();
     console.log("setup res", res);
-    console.log("\n\n\n");
     const devices = await corsair.getDeviceInfo();
-    console.log("Devices", JSON.stringify(devices));
 
     // setInterval(() => {
-    console.log("wow");
-    // const randRGB = createRandomRgb(0, 255);
-    // console.log('randRGB: ', randRGB);
-    // 	let brightness = createRandomRgb(0, 100)[0];
-    // 	console.log('brightness: ', brightness);
+    const randRGB = createRandomRgb(0, 255);
+    console.log("randRGB: ", randRGB);
+    let brightness = createRandomRgb(0, 100)[0];
+    console.log("brightness: ", brightness);
 
-    // 	const state = new LightState().rgb(randRGB).brightness(brightness);
-    // 	devices.map(async (device) => {
-    // 		await corsair.setDeviceState({ devices: [{ type: CorsairDirectTypes.DEVICE, id: device.id }], state });
-    // 	});
+    const state = new LightState().rgb(randRGB).brightness(brightness);
+    devices.map(async (device) => {
+      await corsair.setDeviceState({
+        devices: [{ type: CorsairDirectTypes.DEVICE, id: device.id }],
+        state,
+      });
+    });
     // 	brightness -= 20;
     // }, 5000);
   } catch (err) {
-    console.log("Lights err", err);
+    console.log("Error", err);
     console.log("\n\n\n");
   }
 };
