637f1efa6855edbc8f525dbbd5388f8ef264dfec
ahadcove <me@ahadcove.com>
2023-08-15 10:28:16 -0400

    chore: log to a file

--- app.js
+++ app.js
@@ -1,41 +1,56 @@
-const { CorsairApi } = require('@lumiastream/corsair-cove');
+const { CorsairApi } = require("@lumiastream/corsair-cove");
+const fs = require("fs");
+const util = require("util");
+const log_file = fs.createWriteStream(__dirname + "/debug.log", { flags: "w" });
+const log_stdout = process.stdout;
 // import { createRandomRgb } from '../src/utils';
+
+// Log to a file
+console.log = function (d) {
+  log_file.write(util.format(d) + "\n");
+  log_stdout.write(util.format(d) + "\n");
+};
+console.error = function (d) {
+  log_file.write(util.format(d) + "\n");
+  log_stdout.write(util.format(d) + "\n");
+};
 
 const corsair = new CorsairApi();
 
 const start = async () => {
-	try {
-		const res = await corsair.setup()
-		console.log('setup res', res);
-		console.log('\n\n\n');
-		const devices = await corsair.getDeviceInfo();
-		console.log('Devices', JSON.stringify(devices));
+  try {
+    console.log("Starting");
+    const res = await corsair.setup();
+    console.log("setup res", res);
+    console.log("\n\n\n");
+    const devices = await corsair.getDeviceInfo();
+    console.log("Devices", JSON.stringify(devices));
 
-		setInterval(() => {
-			console.log('wow');
-			// const randRGB = createRandomRgb(0, 255);
-			// console.log('randRGB: ', randRGB);
-		// 	let brightness = createRandomRgb(0, 100)[0];
-		// 	console.log('brightness: ', brightness);
+    // setInterval(() => {
+    console.log("wow");
+    // const randRGB = createRandomRgb(0, 255);
+    // console.log('randRGB: ', randRGB);
+    // 	let brightness = createRandomRgb(0, 100)[0];
+    // 	console.log('brightness: ', brightness);
 
-		// 	const state = new LightState().rgb(randRGB).brightness(brightness);
-		// 	devices.map(async (device) => {
-		// 		await corsair.setDeviceState({ devices: [{ type: CorsairDirectTypes.DEVICE, id: device.id }], state });
-		// 	});
-		// 	brightness -= 20;
-		}, 5000);
-	} catch (err) {
-		console.log('Lights err', err);
-		console.log('\n\n\n');
-	}
+    // 	const state = new LightState().rgb(randRGB).brightness(brightness);
+    // 	devices.map(async (device) => {
+    // 		await corsair.setDeviceState({ devices: [{ type: CorsairDirectTypes.DEVICE, id: device.id }], state });
+    // 	});
+    // 	brightness -= 20;
+    // }, 5000);
+  } catch (err) {
+    console.log("Lights err", err);
+    console.log("\n\n\n");
+  }
 };
 
 start();
 
-process.on('SIGINT', async () => {
-	if (corsair) {
-		CorsairApi.stop();
-	}
+process.on("SIGINT", async () => {
+  if (corsair) {
+    CorsairApi.stop();
+  }
 
-	process.exit(0);
+  process.exit(0);
 });
